e5cc749b495acc4fdfd3d3316096d3ab23f76145
Dayman Novaes <daymannovaes@gmail.com>
2016-09-10 15:10:46 -0300

    foursquare scaffold

---
+++ app/scripts.babel/foursquare-script.js
@@ -0,0 +1,9 @@
+(function() {
+  let callbackUrl = 'https://calendar.google.com';
+  let clientId = 'NCHNPGBJPIRJ44SFBIZYH2CP4G2JYF4OOBMDTYFZQ1L1ZLKD';
+  let baseUrl = 'https://pt.foursquare.com/oauth2/authenticate';
+
+  let authUrl = `${baseUrl}?client_id=${clientId}&response_type=code&redirect_uri=${callbackUrl}`;
+
+
+})();
